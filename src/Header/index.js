78caa4905b34df26d5a39a4f161e0f024b2b58a3
Isaiah Harris <harrisfambam@gmail.com>
2019-02-15 22:23:05 -1000

    fix header

--- src/Header/index.js
+++ src/Header/index.js
@@ -9,6 +9,7 @@ const Header = () => {
         <Link to="/" className="choice">
           <div className="login-choice">Login</div>
         </Link>
+
         <Link to="/timesheet" className="choice">
           <div className="timesheets-choice">Time Sheet</div>
         </Link>
