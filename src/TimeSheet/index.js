abfc29eadfe05750ddfb58d9952801e4e490c0ed
Isaiah Harris <harrisfambam@gmail.com>
2019-02-20 10:12:14 -1000

    add timsheet header

--- src/TimeSheet/index.js
+++ src/TimeSheet/index.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
-import config from '../config';
+
 class TimeSheet extends Component {
-  constructor(props) {
-    super(props);
-
-    this.initClient = this.initClient.bind(this);
-  }
-
-  componentDidMount() {
-    // 1. Load the JavaScript client library.
-    window.gapi.load('client', this.initClient);
-  }
-
-  initClient = () => {
-    // 2. Initialize the JavaScript client library.
-    window.gapi.client
-      .init({
-        apiKey: config.apiKey,
-        // Your API key will be automatically added to the Discovery Document URLs.
-        discoveryDocs: config.discoveryDocs
-      })
-      .then(() => {
-        // 3. Initialize and make the API request.
-        // load(this.onLoad);
-      });
-  };
   render() {
-    return <div />;
+    return (
+      <div className="timesheet-container">
+        <div className="header">timesheet header</div>
+      </div>
+    );
   }
 }
 
--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  apiKey: 'AIzaSyB59TU9RBYMYZVhaK8A5IFwGPYw-SFcFVU',
-  discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-  spreadsheetId: '1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo'
-};
