5ca32c7bcecd3d04de321e410780abd4e0990ff4
Isaiah Harris <harrisfambam@gmail.com>
2019-03-06 13:50:51 -1000

    setup timesheet route file

--- server/routes/timesheet.js
+++ server/routes/timesheet.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const timesheet = require('./timesheet');
 
-router.use('/timesheet', timesheet);
+router.get('/', (req, res) => {
+  console.log('timesheet got');
+});
 module.exports = router;
