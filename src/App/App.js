226099ea315409accd5369c3440e75a9549043ec
Isaiah Harris <harrisfambam@gmail.com>
2019-03-05 20:19:17 -1000

    add header and main components

--- src/App/App.js
+++ src/App/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* header and main components for now */}
         <Header />
         <MainContainer />
       </div>
