72851afdb62b0f7c79845ccfd1cc4ad2bf6de204
Isaiah Harris <harrisfambam@gmail.com>
2019-03-05 20:16:45 -1000

    switch routes for pages

--- src/MainContainer/index.js
+++ src/MainContainer/index.js
@@ -6,6 +6,7 @@ import './MainContainer.scss';
 const MainContainer = () => {
   return (
     <div className="main-container">
+      {/* switch routes for pages */}
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/timesheet" component={TimeSheet} />
