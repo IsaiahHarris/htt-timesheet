ede64569e23cbfdba6fc6dbdba4e8668cbe24d07
Isaiah Harris <harrisfambam@gmail.com>
2019-03-05 20:15:29 -1000

    header component functionality

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 const Header = () => {
+  //header component, would like to add more functionality
   return (
     <header className="Header-header">
-      <img className="logo" src={require('../assets/HTT.png')} alt=""></img>
-      <div className="Header-title">Timesheet
-      </div>
+      <img className="logo" src={require('../assets/HTT.png')} alt="" />
+      <div className="Header-title">Timesheet</div>
       <div className="choices">
         <Link to="/" className="choice">
           <div className="login-choice">Login</div>
