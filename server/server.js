0cd7879782cb26aef05ff3f037c0b3879ca7ec9d
Isaiah Harris <harrisfambam@gmail.com>
2019-03-06 11:12:01 -1000

    server boiler

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
+const routes = require('./routes');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`server is listening on ${PORT}`);
+});
