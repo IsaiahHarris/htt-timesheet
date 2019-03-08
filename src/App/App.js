446e5e708c92f16c34a6981ec8efdfc60232b0a0
Isaiah Harris <harrisfambam@gmail.com>
2019-03-07 19:17:22 -1000

    delete app comment

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* header and main components for now */}
         <Header />
         <MainContainer />
       </div>
